dcc792300f684189f46d45e7be5bd01b5ea3a81f
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-24 10:43:58 -0800

    created code to render the Input component.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,3 +2,14 @@ import {useState} from "react";
 
 // styles
 import styles from "./Input.module.css";
+
+const Input = ({value, onChange, placeholder, styleName, type='text'}) => {
+    const inputStyle = styles[styleName] || styles['default-input'];
+    return (
+        <>
+            <input className={inputStyle} value={value} onChange={onChange} placeholder={placeholder} type={type} />
+        </>
+    );
+};
+
+export default Input;
