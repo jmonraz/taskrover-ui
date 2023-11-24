14d0b8b8d0e1130f1098e652a8239a2f26a6bc7d
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-24 10:57:26 -0800

    added useFormInput custom hook to handle input values.

---
+++ src/hooks/useFormInput.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+export function useFormInput(initialValue = '') {
+    const [value, setValue] = useState(initialValue)
+
+    const handleChange = e => {
+        setValue(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const clearValue = () => setValue('');
+
+    return {value, onChange: handleChange, clearValue: clearValue}
+}
