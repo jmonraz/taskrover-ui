af8e4062da0bb7a62a6ba06f480b8bccc9c47643
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-24 16:18:48 -0800

    added required property to the Input component.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,14 @@
 // styles
 import styles from "./Input.module.css";
 
-// hooks
-import {useFormInput} from "../hooks/useFormInput";
+const Input = ({placeholder, styleName, type='text', inputProps, required=false}) => {
 
-const Input = ({placeholder, styleName, type='text'}) => {
 
-    const inputProps = useFormInput('');
     const inputStyle = styles[styleName] || styles['default-input'];
 
     return (
         <>
-            <input onChange={inputProps.onChange} value={inputProps.value} className={inputStyle} placeholder={placeholder} type={type} />
+            <input onChange={inputProps.onChange} value={inputProps.value} className={inputStyle} placeholder={placeholder} type={type} required={required}/>
         </>
     );
 };
