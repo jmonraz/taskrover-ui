2216fe09c59057644656a59c6f14565a245652f0
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-30 10:11:46 -0800

    added styles to the navbar, still needs work.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,34 @@
 // styles
 import styles from "./Navbar.module.css";
 
+// assets
+import logo from "../assets/logo/taskrover-logo-small.png";
+
 const Navbar = () => {
     return (
         <>
             <div className={styles['navbar']}>
-
+                <div className={styles['navbar-row']}>
+                    <div className={styles['navbar-logo']}>
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div className={styles['navbar-items']}>
+                        <ul>
+                            <li>TICKETS</li>
+                            <li>KNOWLEDGE BASE</li>
+                            <li>CUSTOMERS</li>
+                            <li>ANALYTICS</li>
+                            <li>CHAT</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles['navbar-row']}>
+                    <p>SearchBar</p>
+                    <p>Add button</p>
+                    <p>Notification Box</p>
+                    <p>Gear Icon</p>
+                    <p>User Icon</p>
+                </div>
             </div>
         </>
     );
