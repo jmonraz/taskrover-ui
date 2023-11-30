6e5d23c1787697ec8d251620e28e00a8b4e29028
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-30 09:41:50 -0800

    resolved Input.jsx file after stashing changes from the agent-dashboard branch.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,10 +3,6 @@ import styles from "./Input.module.css";
 
 const Input = ({placeholder, styleName, type='text', inputProps, required=false}) => {
 
-<<<<<<< Updated upstream
-=======
-const Input = ({placeholder, styleName, type='text', inputProps, required=false}) => {
->>>>>>> Stashed changes
 
     const inputStyle = styles[styleName] || styles['default-input'];
 
