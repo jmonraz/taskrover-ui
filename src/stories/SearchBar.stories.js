a064b95257b006f7c23bd3196ee7ae29776f9c39
jorgemonraz <jmonraz@student.fullsail.edu>
2023-11-30 10:32:38 -0800

    added searchbar component.

---
+++ src/stories/SearchBar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+export default {
+    title: "SearchBar",
+    component: SearchBar
+};
+
+export const SearchBarStory = () => <SearchBar />;
