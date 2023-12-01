649f0e28d5d7fa542531a11b57c364a72c963065
jorgemonraz <jmonraz@student.fullsail.edu>
2023-12-01 11:00:19 -0800

    added base structure of the agent dashboard, included dummy ticket blocks to visualize the UI, needs modifications and further testing before production release.

--- src/pages/agent_dashboard/AgentDashboard.jsx
+++ src/pages/agent_dashboard/AgentDashboard.jsx
@@ -1,8 +1,27 @@
 import styles from "./AgentDashboard.module.css";
+
+// components
+import TicketBlock from "../../components/TicketBlock";
 const AgentDashboard = () => {
     return (
         <>
+            <p>FILTER TITLE (number of tickets) v</p>
+            <div>
+                <input type="checkbox" />
+                <p>1 - 30 of 110</p>
+                <button>prev</button>
+                <button>next</button>
+                <hr />
+            </div>
+            <div className={styles['ticket-blocks-col']}>
+                <TicketBlock />
+                <TicketBlock />
+                <TicketBlock />
+                <TicketBlock />
+                <TicketBlock />
+                <TicketBlock />
 
+            </div>
         </>
     );
 };
