a40148788c1bada28218ac9270507076f992125e
jmonraz <jmonraz@student.fullsail.edu>
2023-12-13 13:08:03 -0800

    added submenu for the dot menu on the TicketConversationBlock to show edit and delete tools.

--- src/components/TicketConversationBlock.jsx
+++ src/components/TicketConversationBlock.jsx
@@ -1,11 +1,13 @@
 // styles
 import styles from './TicketConversationBlock.module.css';
 import Button from "./Button";
+import {useState} from "react";
 
 const TicketConversationBlock = ({conversation}) => {
     // const commentWithBreaks = conversation.comment.replace(/\\n/g, "<br />");
+    const [dotMenuClicked, setDotMenuClicked] = useState(false);
     Date.prototype.toString = function () {
-        const options = {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+        const options = {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'};
         return this.toLocaleDateString('en-US', options);
     }
     return (
@@ -16,8 +18,15 @@ const TicketConversationBlock = ({conversation}) => {
                         <p className={styles['conversation-name']}>{conversation.commentOwner}</p>
                         <p className={styles['conversation-date']}>{conversation.commentDate.toDate().toString()}</p>
                     </div>
-                    <div>
-                        <Button styleName='confirm-button'>...</Button>
+                    <div className={styles['button-container']}>
+                        <Button styleName='confirm-button' onClick={() => setDotMenuClicked(!dotMenuClicked)}>...</Button>
+                        {dotMenuClicked &&
+                        <div className={styles['dot-menu']}>
+                            <ul>
+                                <li>Edit</li>
+                                <li>Delete</li>
+                            </ul>
+                        </div>}
                     </div>
                 </div>
                 <p className={styles['conversation-text']} dangerouslySetInnerHTML={{ __html: conversation.comment}} ></p>
