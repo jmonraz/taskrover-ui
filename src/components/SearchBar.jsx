fd58af1b76baf909ad00674deec3010ed95f4c73
Kayle Brown <kaylebrown3825@gmail.com>
2024-01-21 10:48:06 -0500

    The search bar now works, still need a page to display the found item

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,6 +24,7 @@ const SearchBar = () => {
     const handleSearchClick = () => {
         handleSearch(searchQuery);
     };
+
     return(
         <>
             <div className={styles['search-bar']}>
