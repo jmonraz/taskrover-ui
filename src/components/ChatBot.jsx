6cd83b766658ff32a038b4a5febbd8d0bb5ff5c6
jorgemonraz <jmonraz@student.fullsail.edu>
2024-01-23 12:41:32 -0800

    added initial messages to the chat and an option to create a ticket through it.

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,17 +1,33 @@
 import {useState} from "react";
 import styles from "./ChatBot.module.css";
 import chatBotIcon from "../assets/icons/chat_icon.svg";
-const ChatBot = () => {
+const ChatBot = ({messages}) => {
+    const initMessages = [
+        "Hello, how can I help you today?",
+        "Please select from the following options:",
+
+    ];
+
+    const ticketMessages = [
+        "Great, I will gladly help you with creating a ticket!",
+        "Please describe your issue in detail:",
+    ];
+
+    const [optionSelected, setOptionSelected] = useState(false);
 
     const [chatBotOpen, setChatBotOpen] = useState(false);
     const OnHandleClickChat = () => {
         setChatBotOpen(!chatBotOpen);
     }
+
+    const OnHandleClickOption = () => {
+        setOptionSelected(true);
+    }
+
     return (
         <>
             <div className={styles['chat-bubble']} onClick={OnHandleClickChat}>
                 <img src={chatBotIcon} alt="Chat Bot Icon" />
-
             </div>
             {chatBotOpen && (
                 <div className={styles['chat-bot-ctr']}>
@@ -20,7 +36,28 @@ const ChatBot = () => {
                         <p className={styles['chat-bot-close']} onClick={OnHandleClickChat}>X</p>
                     </div>
                     <div className={styles['chat-bot-body']}>
-                        <p>Chat Bot Body</p>
+                        {initMessages.map((message, index) => {
+                            return (
+                                <div className={styles['chat-message']}>
+                                    <p>{message}</p>
+                                </div>
+                            );
+                        })}
+                        {!optionSelected && (
+                            <div className={styles['chat-bot-ctr-ops']}>
+                                <button onClick={OnHandleClickOption}>Create Ticket</button>
+                            </div>
+                        )}
+                        {optionSelected && (
+                            ticketMessages.map((message) => {
+                                return (
+                                    <div className={styles['chat-message']}>
+                                        <p>{message}</p>
+                                    </div>
+                                );
+
+                            })
+                        )}
                     </div>
                     <hr className={styles['horizontal-line']} />
                     <div className={styles['chat-footer']}>
