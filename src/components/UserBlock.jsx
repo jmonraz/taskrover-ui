516930901e7d711413cc20241e930a4056b77714
Kayle Brown <kaylebrown3825@gmail.com>
2024-02-03 18:14:05 -0500

    made a generic profile pic

--- src/components/UserBlock.jsx
+++ src/components/UserBlock.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './UserBlock.module.css';
+import Generic from '../assets/img/Generic-Profile.webp'
 
 const UserBlock = ({ user }) => {
     return (
         <div className={styles['user-block']}>
             <div className={styles['user-info']}>
                 <img
-                    src={user.data().profilePic}
-                    alt="Profile"
+                    src={Generic}
                     className={styles['profile-pic']}
                     onError={(event) => {
+                        event.target.src = "Generic";
                         console.error('Error loading profile picture:', event.error);
                     }}
-                />
+                 alt=''/>
                 <p>{`${user.firstName} ${user.lastName}`} </p>
                 <p>Email: {user.email}</p>
                 <p>Role: {user.role}</p>
