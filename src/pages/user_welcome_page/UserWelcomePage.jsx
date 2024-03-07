794b4392b3fce9c5ab10bbbd71f953765d3494ce
jorgemonraz <jmonraz@student.fullsail.edu>
2024-03-07 13:46:21 -0800

    added an image and message to the user welcoming page.

--- src/pages/user_welcome_page/UserWelcomePage.jsx
+++ src/pages/user_welcome_page/UserWelcomePage.jsx
@@ -1,6 +1,9 @@
 import styles from "./UserWelcomePage.module.css";
-import SearchBar from "../../components/SearchBar";
 import ChatBot from "../../components/ChatBot";
+import Button from "../../components/Button";
+
+// assets
+import AgentImage from "../../assets/img/virtual-assistant.svg";
 
 const UserWelcomePage = () => {
     return (
@@ -14,11 +17,21 @@ const UserWelcomePage = () => {
                     </div>
                 </div>
                 <div className={styles['row']}>
-                </div>
-                <div className={styles['row']}>
+                    <div className={styles['row-ch']}>
+                        <div className={styles['agent-ctr']}>
+                            <img src={AgentImage} alt="agent"/>
+                        </div>
+                    </div>
+                    <div className={styles['row-ch']}>
+                        <div className={styles['col-ch']}>
+                            <p>Need assistance? Let's fix it! Start by submitting your ticket here.</p>
+                            <Button onClick={() => {
+                            }} styleName='green-button'>GET STARTED</Button>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles['chat']}>
-                    <ChatBot />
+                    <ChatBot/>
                 </div>
             </div>
         </>
